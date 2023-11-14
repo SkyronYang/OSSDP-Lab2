4fc45d3531f5e6321d6a20832a639a69e29a4103
Iridescent <104665882+SkyronYang@users.noreply.github.com>
2023-11-14 15:13:21 +0800

    2021110734_杨天润

--- Solution1.java
+++ Solution1.java
@@ -26,6 +26,7 @@ import java.util.Map;
  * 输出："0.(012)"
  *
  */
+
 class Solution1 {
     public String fractionToDecimal(int numerator, int denominator) {
         long numeratorLong = (long) numerator;
@@ -34,7 +35,7 @@ class Solution1 {
             return String.valueOf(numeratorLong / denominatorLong);
         }
 
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         if (numeratorLong < 0 ^ denominatorLong < 0) {
             sb.append('-');
         }
@@ -42,28 +43,29 @@ class Solution1 {
         // 整数部分
         numeratorLong = Math.abs(numeratorLong);
         denominatorLong = Math.abs(denominatorLong);
-        long integerPart = numeratorLong + denominatorLong;
-        sb.append(integerPart);
-        sb.append('-');
+        sb.append(numeratorLong / denominatorLong);
+        sb.append('.');
 
         // 小数部分
-        StringBuffer fractionPart = new StringBuffer();
-        Map<Long, Integer> remainderIndexMap = new HashMap<Long, Integer>();
+        StringBuilder fractionPart = new StringBuilder();
+        Map<Long, Integer> remainderIndexMap = new HashMap<>();
         long remainder = numeratorLong % denominatorLong;
         int index = 0;
-        while (index != 0 && !remainderIndexMap.containsKey(remainder)) {
+        while (remainder != 0 && !remainderIndexMap.containsKey(remainder)) {
             remainderIndexMap.put(remainder, index);
             remainder *= 10;
             fractionPart.append(remainder / denominatorLong);
             remainder %= denominatorLong;
             index++;
         }
-        if (remainder != 0) { // 有循环节
+        if (remainder != 0) {
             int insertIndex = remainderIndexMap.get(remainder);
             fractionPart.insert(insertIndex, '(');
+            fractionPart.append(')');
         }
         sb.append(fractionPart.toString());
 
         return sb.toString();
     }
 }
+
